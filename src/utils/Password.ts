63d4664db524a0b9ffa7769ad563b843cad5d71b
Felipe Cacezi <felipecacezi@gmail.com>
2024-10-12 16:56:49 -0300

    feat: implementando classe de utilidade de Password

---
+++ src/utils/Password.ts
@@ -0,0 +1,17 @@
+﻿import * as bcrypt from 'bcrypt';
+
+export class Password {
+  private saltRounds: number;
+
+  constructor(saltRounds: number = 10) {
+    this.saltRounds = saltRounds;
+  }
+
+  async generateHash(senha: string): Promise<string> {
+    return await bcrypt.hash(senha, this.saltRounds);
+  }
+
+  async compareHash(senhaInformada: string, senhaArmazenada: string): Promise<boolean> {
+    return await bcrypt.compare(senhaInformada, senhaArmazenada);
+  }
+}
